caece0bdd8d5e1c0850cc23f00c5be08a3cef746
sidharthpadhi02 <sidharthsitun1504@outlook.com>
2021-07-15 23:40:30 +0530

    MyMazeGamePrototype

---
+++ maze game/Assets/MyOwn Scripts/MapSawnScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapSawnScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerPos;
+    [SerializeField]
+    private Transform offScreenPos;
+    public float speed;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+        }
+    }
+    
+}
